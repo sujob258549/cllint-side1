e4253eea2f5d63d424611c1bbc9f67601dc2ad64
DevSujon <sujonmaia274@gmail.com>
2024-04-22 21:38:46 +0600

    delete cod

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
+import { useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [userss, setusers] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setusers(data))
+  }, [])
+  console.log(userss)
+
+  const handelsubmit = e => {
+    e.preventDefault()
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const users = { name, email }
+    console.log(users)
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(users)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        if (data.insertedId) {
+          alert('add to clint');
+
+        }
+      })
+  }
+
+
+
+  //   fetch('http://localhost:3000/users', {
+  //     method: 'POST',
+  //     body: JSON.stringify(users),
+  //     headers: {
+  //       'Content-type': 'application/json; charset=UTF-8',
+  //     },
+  //   })
+  //     .then((response) => response.json())
+  //     .then((json) => console.log(json));
+  // }
+
+
+  // const handeldelate = _id => {
+  //   console.log(_id);
+    // fetch(`http://localhost:3000/users/${_id}`, {
+    //   method: 'DELETE'
+    // })
+  //     .then(res => res.json())
+  //     .then(data => console.log(data));
+  // }
+
+
+  const handeldelate = _id => {
+    console.log(_id)
+    fetch(`http://localhost:5000/users/${_id}`, {
+      method: 'DELETE'
+    })
+    
+      .then(res => res.json())
+      .then((json) => console.log(json));
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Mongodb project</h1>
+      <form onSubmit={handelsubmit}>
+        <input type="text" name="name" id="" /><br />
+        <input type="email" name="email" id="" /><br />
+        <input type="submit" value={"Submit"} id="" />
+
+
+      </form>
+
+
+      {
+        userss.map(user => (
+          <div key={user.insertedId} style={{ display: 'flex', flex: 'worp', gap: '50px', border: '1px solid', padding: '10px', margin: '10px' }}>
+            <h1>{user.insertedId}</h1>
+            <h2><span style={{ marginRight: "10px" }}> Name</span> {user.name}</h2>
+            <h2><span style={{ marginRight: "10px" }}> Email</span> {user.email}</h2>
+            <h2><span style={{ marginRight: "10px" }}> id</span> {user._id}</h2>
+            <button onClick={() => handeldelate(user._id)} style={{ border: '2px silid' }}>Delate buttons</button>
+          </div>
+        ))
+      }
+
     </>
   )
 }
